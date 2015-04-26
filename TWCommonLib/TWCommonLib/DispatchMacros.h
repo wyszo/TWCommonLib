892e6067fcee1e06b1ae13ada2fbc313fa08c2e9
Tomasz Wyszomirski <thomas@wyszomirski.co.uk>
2015-04-27 01:15:03 +0200

    DispatchMacros added

---
+++ TWCommonLib/TWCommonLib/DispatchMacros.h
@@ -0,0 +1,20 @@
+//
+//  TWCommonLib
+//
+
+#ifndef DispatchMacros_h
+#define DispatchMacros_h
+
+typedef NS_ENUM(long, QueuePriority) {
+  QueuePriorityHigh = DISPATCH_QUEUE_PRIORITY_HIGH,
+  QueuePriorityLow = DISPATCH_QUEUE_PRIORITY_LOW,
+  QueuePriorityBackground = DISPATCH_QUEUE_PRIORITY_BACKGROUND,
+  QueuePriorityDefault = DISPATCH_QUEUE_PRIORITY_DEFAULT
+};
+
+
+extern void DISPATCH_AFTER(NSTimeInterval when, void (^block)());
+extern void DISPATCH_ASYNC(QueuePriority priority, void (^block)());
+extern void DISPATCH_ASYNC_ON_MAIN_THREAD(void (^block)());
+
+#endif
