c1b3527b0d640c299d294726c6a1e0a0bea43eb7
Tomasz Wyszomirski <thomas@wyszomirski.co.uk>
2015-04-29 18:39:50 +0200

    Added a macro for safe block calling

--- TWCommonLib/TWCommonLib/CommonMacros.h
+++ TWCommonLib/TWCommonLib/CommonMacros.h
@@ -6,11 +6,16 @@
 #define TWCommonLib_CommonMacros_h
 
 
+// Inside methods
+#define CallBlock(block, ...) (block ? block(__VA_ARGS__) : nil);
 #define defineWeakSelf() __weak typeof(self) weakSelf = self
 
-#define NEW_AND_INIT_UNAVAILABLE - (instancetype)new __unavailable; - (instancetype)init __unavailable;
-
+// Inside methods - assertions
 #define NOT_IMPLEMENTED_YET_RETURN AssertTrueOrReturn(NO && @"Not implemented yet");
 #define NOT_IMPLEMENTED_YET_RETURN_NIL AssertTrueOrReturnNil(NO && @"Not implemented yet");
+
+// Inside class interface definition
+#define NEW_AND_INIT_UNAVAILABLE - (instancetype)new __unavailable; - (instancetype)init __unavailable;
+
 
 #endif
