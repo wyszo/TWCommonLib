21400cd75618d4338fbb94c1c024429c3360b923
Tomasz Wyszomirski <thomas@wyszomirski.co.uk>
2015-05-01 19:46:21 +0200

    Tests target added to the project

---
+++ TWCommonLib/TWCommonLib Tests/TWCommonLib_Tests.m
@@ -0,0 +1,19 @@
+//
+//  TWCommonLib Tests
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface TWCommonLib_Tests : XCTestCase
+
+@end
+
+@implementation TWCommonLib_Tests
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+@end
