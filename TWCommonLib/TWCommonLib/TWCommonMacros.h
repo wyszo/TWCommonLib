cb562106b2b4312884552ff82387221ea46d3788
Tomasz Wyszomirski <thomas@wyszomirski.co.uk>
2015-05-05 03:23:14 +0200

    SuppressPerformSelectorLeakWarning macro added

--- TWCommonLib/TWCommonLib/TWCommonMacros.h
+++ TWCommonLib/TWCommonLib/TWCommonMacros.h
@@ -10,6 +10,15 @@
 #define CallBlock(block, ...) (block ? block(__VA_ARGS__) : nil);
 #define defineWeakSelf() __weak typeof(self) weakSelf = self
 
+#define SuppressPerformSelectorLeakWarning(Code) \
+do { \
+_Pragma("clang diagnostic push") \
+_Pragma("clang diagnostic ignored \"-Warc-performSelector-leaks\"") \
+Code; \
+_Pragma("clang diagnostic pop") \
+} while (0)
+
+
 // Inside methods - assertions
 #define NOT_IMPLEMENTED_YET_RETURN AssertTrueOrReturn(NO && @"Not implemented yet");
 #define NOT_IMPLEMENTED_YET_RETURN_NIL AssertTrueOrReturnNil(NO && @"Not implemented yet");
