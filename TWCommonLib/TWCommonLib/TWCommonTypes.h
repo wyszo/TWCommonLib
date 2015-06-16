71eeba6ee250c9293e4b4776b299a88ccb780699
Tomasz Wyszomirski <thomas@wyszomirski.co.uk>
2015-06-16 13:44:51 +0200

    CollectionViewCellAtIndexPathBlock type declared

--- TWCommonLib/TWCommonLib/TWCommonTypes.h
+++ TWCommonLib/TWCommonLib/TWCommonTypes.h
@@ -12,6 +12,7 @@ typedef void (^VoidBlock)();
 typedef void (^BlockWithBoolParameter)(BOOL);
 typedef void (^BlockWithFloatParameter)(CGFloat);
 typedef void (^CellAtIndexPathBlock)(UITableViewCell *cell, NSIndexPath *indexPath);
+typedef void (^CollectionViewCellAtIndexPathBlock)(UICollectionViewCell *cell, NSIndexPath *indexPath);
 typedef void (^IndexPathBlock)(NSIndexPath *indexPath);
 
 
