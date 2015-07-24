57a5b412108bb28a9a6e17768c214a5d9ae38856
Tomasz Wyszomirski <thomas@wyszomirski.co.uk>
2015-07-24 15:10:20 +0200

    docs: todo item added

--- TWCommonLib/TWCommonLib/TWCoreDataTableViewDataSource.m
+++ TWCommonLib/TWCommonLib/TWCoreDataTableViewDataSource.m
@@ -29,6 +29,7 @@
   
   self = [super init];
   if (self) {
+    // TODO: this won't copy the block (since it's assigned to an array)! bug!
     _cellReuseMappingBlock = cellReuseMappingBlock;
     _configureCellBlock = configureCellBlock;
   }
