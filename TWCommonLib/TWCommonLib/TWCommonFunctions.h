a9eae2e1ec7410d29eb7abccb1084cb355090cfa
Tomasz Wyszomirski <thomas@wyszomirski.co.uk>
2015-07-31 16:12:38 +0200

    feat: valueInRange function added

--- TWCommonLib/TWCommonLib/TWCommonFunctions.h
+++ TWCommonLib/TWCommonLib/TWCommonFunctions.h
@@ -6,6 +6,9 @@
 #ifndef __TWCommonLib__TWCommonFunctions__
 #define __TWCommonLib__TWCommonFunctions__
 
-CGRect CGRectFromSize(CGSize size);
+// Returns CGRect starting from (0,0) with a given size
+FOUNDATION_EXPORT CGRect CGRectFromSize(CGSize size);
+
+FOUNDATION_EXPORT CGFloat valueInRange(CGFloat min, CGFloat max, CGFloat value);
 
 #endif
