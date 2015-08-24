f513067cbe58933f35c08708789c2e433f45ed37
Tomasz Wyszomirski <thomas@wyszomirski.co.uk>
2015-08-24 17:33:55 +0200

    feat: NSLayoutConstraint animation method extended with a completion block

--- TWCommonLib/TWCommonLib/NSLayoutConstraint+TWAnimations.m
+++ TWCommonLib/TWCommonLib/NSLayoutConstraint+TWAnimations.m
@@ -3,17 +3,24 @@
 //
 
 #import "NSLayoutConstraint+TWAnimations.h"
+#import "TWCommonMacros.h"
 
 
 @implementation NSLayoutConstraint (TWAnimations)
 
-- (void)animateToValue:(CGFloat)destinationValue onView:(UIView *)view withDuration:(NSTimeInterval)animationDuration
+- (void)animateToValue:(CGFloat)destinationValue onView:(UIView *)view withDuration:(NSTimeInterval)animationDuration {
+    [self animateToValue:destinationValue onView:view withDuration:animationDuration completion:nil];
+}
+
+- (void)animateToValue:(CGFloat)destinationValue onView:(UIView *)view withDuration:(NSTimeInterval)animationDuration completion:(BlockWithBoolParameter)completion
 {
-  self.constant = destinationValue;
-  
-  [UIView animateWithDuration:animationDuration animations:^{
-    [view layoutIfNeeded];
-  }];
+    self.constant = destinationValue;
+    
+    [UIView animateWithDuration:animationDuration animations:^{
+        [view layoutIfNeeded];
+    } completion:^(BOOL finished) {
+        CallBlock(completion, finished);
+    }];
 }
 
 @end
