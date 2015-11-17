5baa0d7e0353bf5d0560f9d12f68b231b052e662
Tomasz Wyszomirski <thomas@wyszomirski.co.uk>
2015-11-17 19:37:14 +0100

    feat: UIView tw_top and tw_bottom methods added

--- TWCommonLib/TWCommonLib/UIView+TWSizes.h
+++ TWCommonLib/TWCommonLib/UIView+TWSizes.h
@@ -9,6 +9,8 @@
  */
 @interface UIView (TWSizes)
 
+@property (nonatomic, assign) CGFloat tw_top;
+@property (nonatomic, assign) CGFloat tw_bottom;
 @property (nonatomic, assign) CGFloat tw_width;
 @property (nonatomic, assign) CGFloat tw_height;
 
--- TWCommonLib/TWCommonLib/UIView+TWSizes.m
+++ TWCommonLib/TWCommonLib/UIView+TWSizes.m
@@ -6,6 +6,30 @@
 
 @implementation UIView (TWSizes)
 
+- (CGFloat)tw_top
+{
+  return self.frame.origin.y;
+}
+
+- (void)setTw_top:(CGFloat)tw_top
+{
+  CGRect frame = self.frame;
+  frame.origin.y = tw_top;
+  self.frame = frame;
+}
+
+- (CGFloat)tw_bottom
+{
+  return ([self tw_top] + self.frame.size.height);
+}
+
+- (void)setTw_bottom:(CGFloat)tw_bottom
+{
+  CGRect frame = self.frame;
+  frame.origin.y = (tw_bottom - frame.size.height);
+  self.frame = frame;
+}
+
 - (CGFloat)tw_width
 {
   return self.frame.size.width;
