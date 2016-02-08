d4f58c8ddaa8fc52f6d408b0ce9e03f47ba540f4
Tomasz Wyszomirski <thomas@wyszomirski.co.uk>
2016-02-08 12:17:21 +0100

    feat: DispatchSyncOnMainThread Swift implementation added

--- TWCommonLib/TWCommonLib/DispatchFunctions.swift
+++ TWCommonLib/TWCommonLib/DispatchFunctions.swift
@@ -4,3 +4,13 @@ public func DispatchAfter(time: Double, closure: ()->()) {
     let delayTime = dispatch_time(DISPATCH_TIME_NOW, Int64(time * Double(NSEC_PER_SEC)))
     dispatch_after(delayTime, dispatch_get_main_queue(), closure)
 }
+
+public func DispatchSyncOnMainThread(closure: ()->()) {
+    if NSThread.isMainThread() {
+        closure()
+    } else {
+        dispatch_sync(dispatch_get_main_queue()) {
+            closure()
+        }
+    }
+}
