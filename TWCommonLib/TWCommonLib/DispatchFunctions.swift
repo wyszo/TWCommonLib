75ce6ca0ce1367fef62c9ff08f9102b72115e0c4
Tomasz Wyszomirski <thomas@wyszomirski.co.uk>
2016-02-23 14:07:30 +0100

    feat: DispatchAsyncOnMainThread method added

--- TWCommonLib/TWCommonLib/DispatchFunctions.swift
+++ TWCommonLib/TWCommonLib/DispatchFunctions.swift
@@ -9,8 +9,10 @@ public func DispatchSyncOnMainThread(closure: ()->()) {
     if NSThread.isMainThread() {
         closure()
     } else {
-        dispatch_sync(dispatch_get_main_queue()) {
-            closure()
-        }
+        dispatch_sync(dispatch_get_main_queue(), closure)
     }
+}
+
+public func DispatchAsyncOnMainThread(closure: ()->()) {
+  dispatch_async(dispatch_get_main_queue(), closure)
 }
